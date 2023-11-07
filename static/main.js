123b6e3aacadc05454a7f03dfe1451d767459ba3
galactixx <nwlezien.github@gmail.com>
2023-11-07 01:21:47 -0600

    clean-up

--- static/main.js
+++ static/main.js
@@ -1,10 +1,5 @@
 var board, game = new Chess();
 
-function handlePromotion(target, piece) {
-  var newPiece = prompt('Promotion! Choose a piece (q, r, b, n)');
-  game.put({type: newPiece, color: piece.color}, target);
-}
-
 function onSnapPiece() {
   board.position(game.fen());
 }
@@ -26,9 +21,10 @@ function movePiece(source, target, newPiece) {
 
 function onDropPiece(source, target) {
   var piece = game.get(source);
+  
   if (piece.type === 'p' && (target[1] === '8' || target[1] === '1')) {
-    var newPiece = prompt('Promotion! Choose a piece (q, r, b, n)');
-    movePiece(source, target, newPiece);
+      var newPiece = prompt('Promotion! Choose a piece (q, r, b, n)');
+      movePiece(source, target, newPiece);
   } else {
     movePiece(source, target, 'q');
   }
