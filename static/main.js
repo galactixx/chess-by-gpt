e0cc79f7dd3d8e229c79939fe2a32e68d84b0e11
galactixx <nwlezien.github@gmail.com>
2023-11-08 00:46:32 -0600

    finish notation logic

--- static/main.js
+++ static/main.js
@@ -1,8 +1,16 @@
 var board, game = new Chess();
-var movesWhite = [];
 
-window.onload = function() {
-  document.getElementById("white moves").innerHTML = movesWhite.join(", ");
+var movesWhite = [];
+var movesBlack = [];
+
+function appendMoves(id, moveList) {
+  var listHTML = "<ul>";
+
+  for (var i=0; i<moveList.length; i++) {
+    listHTML += "<li>" + moveList[i] + "</li>";
+  }
+  listHTML += "</ul>";
+  document.getElementById(id).innerHTML = listHTML;
 }
 
 function onSnapPiece() {
@@ -15,19 +23,42 @@ function illegalMove(move) {
   }
 }
 
-function movePiece(source, target, newPiece) {
+function movePiece(source, target, piece, newPiece) {
   var move = game.move({
     from: source,
     to: target,
     promotion: newPiece
   });
   illegalMove(move);
+
+  if (move != null) {
+    if (piece.type == 'p' && move.flags.includes('c')) {
+      var pieceAdd = source[0].concat('x').concat(target);
+    } else if (piece.type === 'p') {
+      var pieceAdd = target;
+    } else if (move.flags.includes('c')) {
+      var pieceAdd = piece.type.toUpperCase().concat('x').concat(target);
+    } else {
+      var pieceAdd = piece.type.toUpperCase().concat(target);
+    }
+  if (game.in_check()) {
+    pieceAdd = pieceAdd.concat('+')
+  }
+
+    if (game.turn() === 'b') {
+      movesWhite.push(pieceAdd);
+      appendMoves('white-moves', movesWhite);
+    } else {
+      movesBlack.push(pieceAdd);
+      appendMoves('black-moves', movesBlack);
+    }
+  }
 }
 
-function promotionPieceSelected(id, piece, source, target) {
+function promotionPieceSelected(id, piece, piece_promotion, source, target) {
 
   document.getElementById(id).addEventListener('click', function() {
-    movePiece(source, target, piece);
+    movePiece(source, target, piece, piece_promotion);
     document.getElementById('promotion').classList.add('hide');
     board.position(game.fen());
   });
@@ -40,33 +71,23 @@ function onDropPiece(source, target) {
       document.getElementById('promotion').classList.remove('hide');
       document.getElementById('promotion').classList.add('show');
 
-      promotionPieceSelected('queen', 'q', source, target);
-      promotionPieceSelected('rook', 'r', source, target);
-      promotionPieceSelected('bishop', 'b', source, target);
-      promotionPieceSelected('knight', 'n', source, target);
+      promotionPieceSelected('queen', piece, 'q', source, target);
+      promotionPieceSelected('rook', piece, 'r', source, target);
+      promotionPieceSelected('bishop', piece, 'b', source, target);
+      promotionPieceSelected('knight', piece, 'n', source, target);
   } else {
-    movePiece(source, target, 'q');
-  }
-
-  if (game.turn() === 'b') {
-    if (piece.type === 'p') {
-      var pieceAdd = target
-    } else {
-      var pieceAdd = piece.type.toUpperCase().concat(target)
-    }
-    movesWhite.push(pieceAdd);
-    document.getElementById("white moves").innerHTML = movesWhite.join(", ");
+    movePiece(source, target, piece, 'q');
   }
 }
 
 function onDragPiece (source, piece, position, orientation) {
   if (game.game_over()) {
-    return false
+    return false;
   };
 
   if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
       (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-    return false
+    return false;
   };
 }
 
